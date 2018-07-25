2fd5043b1c617b7ad83babe37a2724e166f83e23
Елена Анищенкова <anischenkovaev@altarix.ru>
2018-07-25 13:17:45 +0300

    class VerticalFireball is ready

--- game.js
+++ game.js
@@ -318,6 +318,12 @@ class HorizontalFireball extends Fireball {
     }
 }
 
+class VerticalFireball extends Fireball {
+    constructor(pos = new Vector(0, 0)) {
+        super(pos, new Vector(0, 2));
+    }
+}
+
 class Player extends Actor {
     constructor(pos = new Vector(0, 0)) {
         super(pos.plus(new Vector(0, -0.5)), new Vector(0.8, 1.5));
